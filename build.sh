c4dbf54e4a4fd3604f2f6b8f3236573719d12cdb
Ubuntu <ubuntu@ip-172-31-32-91.eu-west-3.compute.internal>
2024-04-22 12:10:50 +0000

    updated= buildspec file

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 #export TAG=$1
 #export CONTAINER_NAME=$2
 export REPO_URL=$1
+export LOCAL_PATH=/home/ubuntu/app-git
 echo "Start building image"
 echo "Hostname = "`hostname`
 echo "REPO_URL = $REPO_URL"
+cd $LOCAL_PATH
 docker build -t $REPO_URL:latest .
