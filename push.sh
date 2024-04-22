0c7643a83cfd30a889eddca37c4215ad4c57aeb2
Ubuntu <ubuntu@ip-172-31-32-91.eu-west-3.compute.internal>
2024-04-22 12:38:35 +0000

    updated= buildspec file

--- push.sh
+++ push.sh
@@ -4,10 +4,11 @@
 export REPO_URL=$1
 export IMAGE_TAG=$2
 export LOCAL_PATH=/home/ubuntu/app-git
-echo "Start building image"
+echo "Start pushing image"
 echo "Hostname = "`hostname`
 echo "REPO_URL = $REPO_URL"
 cd $LOCAL_PATH
 echo "CURRENT DIRECTORY="`pwd`
-docker build -t $REPO_URL:latest .
-docker tag $REPO_URL:latest $REPO_URL:$IMAGE_TAG
+
+docker push $REPO_URL:$IMAGE_TAG
+docker push $REPO_URL:latest
