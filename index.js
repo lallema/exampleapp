a07cd5474185444580748eed2b3eceffb48bd125
Ubuntu <ubuntu@ip-172-31-32-91.eu-west-3.compute.internal>
2024-04-22 08:27:50 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.json({message: 'Hellow World AWS Tho a dit: Helloww ca se termine pas par W try 1'})
+})
+let server  = app.listen(port, () => {
+  console.log(`example app listening at port ${port}`)
+})
+function stop() {
+  server.close();
+}
+module.export = server;
+module.export.stop = stop;
